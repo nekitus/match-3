c3611a46149441d7d5c4819381052240fdda0382
nikita <nikita@MacBook-Pro-1.local>
2017-06-27 12:39:16 +0300

    fixes in view

--- src/app/counter/View.js
+++ src/app/counter/View.js
@@ -1,10 +1,9 @@
-export default class Counter {
+import View from '../../lib/View'
+export default class Counter extends View {
     constructor(props) {
+        super(props);
         this.onChange = this.onChange.bind(this);
 
-        this.props = props;
-        this.model = props.model;
-        this.container = props.container;
         this.position = {};
         this.display = true;
 
@@ -12,16 +11,15 @@ export default class Counter {
         this.graphics.x = 30;
         this.graphics.y = 240;
         this.render();
-        this.container.addChild(this.graphics);
-
-        this.model.bind("summ", this.onChange)
+        this.props.container.addChild(this.graphics);
+        this.props.model.bind("summ", this.onChange)
     }
     onChange(pointsArray) {
         this.render()
     }
     render() {
         if(this.display) {
-            this.graphics.text = "Очки: " + this.model.getPoints() || 0;
+            this.graphics.text = "Очки: " + this.props.model.getPoints() || 0;
         }
     }
 }
